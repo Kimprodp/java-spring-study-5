d188a5cb317aeed5f496e78555760f1a8e2a2995
Gyutae Kim <kiimwf0819@gmail.com>
2024-02-12 11:33:16 +0900

    섹션1. 프로젝트 생성

---
+++ src/test/java/fourthproject/thymeleaf/ThymeleafStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package fourthproject.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
