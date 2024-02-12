d854c192332441369abcf9c7f8c6ed7f7781caf8
Gyutae Kim <kiimwf0819@gmail.com>
2024-02-12 15:33:14 +0900

    섹션1. 변수 - SpringEL

--- src/main/java/fourthproject/thymeleaf/basic/BasicController.java
+++ src/main/java/fourthproject/thymeleaf/basic/BasicController.java
@@ -1,5 +1,11 @@
 package fourthproject.thymeleaf.basic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,5 +25,32 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "Hello <b>Spring</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User user1 = new User("userA", 20);
+        User user2 = new User("userB", 30);
+
+        List<User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", user1);
+        map.put("userB", user1);
+
+        model.addAttribute("user", user1);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class User {
+        private String username;
+        private int age;
     }
 }
